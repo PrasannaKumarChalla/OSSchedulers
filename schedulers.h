3757cf07ce1b10b36b9972f1daddca130060263e
s521942 <kumar.challa7893@gmail.com>
2015-11-08 16:41:07 -0600

    Added definitions

--- schedulers.h
+++ schedulers.h
@@ -1,5 +1,7 @@
 #ifndef SOLN_SCHEDULERS_H
 #define SOLN_SCHEDULERS_H
-
-
+int fcfs(proc_t * processes, int numprocesses, int timeslice);
+int sjf(proc_t * processes, int numprocesses, int timeslice);
+int srt(proc_t * processes, int numprocesses, int timeslice);
+int rr(proc_t * processes, int numprocesses, int timeslice);
 #endif //SOLN_SCHEDULERS_H
